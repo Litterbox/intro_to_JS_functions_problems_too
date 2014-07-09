5900312873a37ce605d514ff08e75f3a44c87085
Tim Licata <tlicata@hotpads.com>
2014-07-08 21:37:35 -0700

    part3

---
+++ part3.js
@@ -0,0 +1,28 @@
+// Write a function that takes one argument,
+// someone's name, and returns a string that
+// says hello to them.
+
+// Hint: this function will look similar to
+// the function in part two, except is will
+// NOT use console.log(). Instead it will use
+// the return keyword.
+
+// YOUR FUNCTION HERE
+var makeGreeting = function (name) {
+    return "Hello, " + name + "!!";
+};
+
+
+// LEAVE EVERYTHING BELOW HERE ALONE
+var helloFrank = makeGreeting("Frank");
+var helloAlice = makeGreeting("Alice");
+if (helloFrank === "Hello, Frank!") {
+    console.log("Congratulations, you greeted Frank");
+} else {
+    console.log("Uh oh, you didn't greet Frank :(");
+}
+if (helloAlice === "Hello, Alice!") {
+    console.log("Congratulations, you greeted Alice");
+} else {
+    console.log("Uh oh, you didn't greet Alice :(");
+}
