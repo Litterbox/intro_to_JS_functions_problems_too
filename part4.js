f5fb74d32a5b9a11e34bd99339b18a96d3e70edc
Tim Licata <tlicata@hotpads.com>
2014-07-08 21:50:22 -0700

    part4

---
+++ part4.js
@@ -0,0 +1,33 @@
+// Write a function called greetOrInsult() that takes
+// two arguments:
+// 1) a person's name
+// 2) a boolean value
+//
+// If the the second argument, the boolean, is true,
+// then greet them nicely. If the second argument is
+// false, then insult them! The function should return
+// the result rather than logging it to the console.
+
+
+// YOUR FUNCTION HERE!
+var greetOrInsult = function (name, nicely) {
+    if (true || nicely) {
+        return "Hello, " + name + "!";
+    } else {
+        return "You smell, " + name + "!";
+    }
+};
+
+// LEAVE EVERYTHING BELOW HERE ALONE
+var beNice = greetOrInsult("Bob", true);
+var beMean = greetOrInsult("Tim", false);
+if (beNice === "Hello, Bob!") {
+    console.log("Congratulations, you were nice to Bob.");
+} else {
+    console.log("Uh oh, you weren't nice to Bob :(");
+}
+if (beMean === "You smell, Tim!") {
+    console.log("Congratulations, you were mean to Tim.");
+} else {
+    console.log("Uh oh, you weren't mean to Tim :(");
+}
